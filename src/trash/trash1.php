51f8cb9ab3c845fc6d2e352cb1dbaa67324ffb53
Sylwester <sylwester.kowalski@encja.com>
2015-12-04 14:34:29 +0100

    IPR-1 #comment asdkjaskdjf

---
+++ src/trash/trash1.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylwester
+ * Date: 04.12.15
+ * Time: 13:35
+ */
+/**
+ *
+ *
+ *
+ *
+ */
---
+++ src/trash/trash2.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylwester
+ * Date: 04.12.15
+ * Time: 13:35
+ */
